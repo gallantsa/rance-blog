56cb5e5a0082603375e10da125b45c55acf380dd
gallantsa <gallantsa@163.com>
2026-06-20 04:03:28 +0800

    Django的接客之道

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客',
+        'welcome': '欢迎来到我的博客首页'
+    })
