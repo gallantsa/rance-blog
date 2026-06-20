85aeea9141e015a5a57142e8fb47cc36b6e7790f
gallantsa <gallantsa@163.com>
2026-06-20 14:01:56 +0800

    为文章详情视图添加目录支持，优化Markdown解析

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post
+import re
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+
 
 def index(request):
     # - 代表逆序
@@ -12,10 +15,16 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body, extensions=[
+    md = markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
     ])
+
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
 
     return render(request, 'blog/detail.html', context={'post': post})
