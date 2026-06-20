3663d4980a7c7d25e28519c66ba4c943cd96e87d
gallantsa <gallantsa@163.com>
2026-06-20 13:11:07 +0800

    重构博客页面，添加文章详情视图和模板，优化导航结构

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -82,10 +83,15 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
         super().save(*args, **kwargs)
-        
+
     class Meta:
         verbose_name = '文章'
         verbose_name_plural = verbose_name
+
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
