c70a8553a74e753f7a5b772427d7293c3f4fbec2
gallantsa <gallantsa@163.com>
2026-06-21 16:11:07 +0800

    优化分页功能，添加简单分页逻辑并更新视图以支持分页

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,7 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
+    paginate_by = 10
 
 # 记得在顶部导入 DetailView
 class PostDetailView(DetailView):
