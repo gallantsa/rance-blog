b17c514a9906d3081e683a5edcbaf18437c665d4
gallantsa <gallantsa@163.com>
2026-06-21 15:43:52 +0800

    重构视图和路由，使用类视图替代函数视图，优化文章详情和归档展示

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('post/<int:pk>/', views.detail, name='detail'),
-    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
-    path('categories/<int:pk>/', views.category, name='category'),
-    path('tags/<int:pk>/', views.tag, name='tag'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('archives/<int:year>/<int:month>/', views.ArchiveView.as_view(), name='archives'),
+    path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    path('tag/<int:pk>/', views.TagView.as_view(), name='tag'),
 
 ]
